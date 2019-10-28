a43cc5a5da7a1aabb56acd58b1781a9a0e9cae5c
Uriel Peña Estel <39961859+UrielPE@users.noreply.github.com>
2019-10-28 09:21:16 -0600

    Update localization.js

--- Skill Code/lambda/localization.js
+++ Skill Code/lambda/localization.js
@@ -1,15 +1,17 @@
 module.exports = {
     es: {
         translation:{
-             WELCOME_MESSAGE: 'Bienvenido, si no sabes o no recuerdas las reglas o las categorías, puedes preguntarlas, si ya lo sabes, comencemos, pero no olvides seleccionar una categoría.',
+            AY_AY_AY_SPEECHCON: `<say-as interpret-as="interjection">ay ay ay</say-as>`,
+             WELCOME_MESSAGE: 'Bienvenido %s , si no sabes o no recuerdas las reglas o las categorías, puedes preguntarlas, si ya lo sabes, comencemos, pero no olvides seleccionar una categoría.',
              RULES_MESSAGE: `Estas son las reglas, primero yo iniciaré mencionando la siguiente frase,
-                caricachupas presenta nombres de. enseguida tú tendrás que mencionar una categoría.
-                Por último si repites la palabra,o no está en la categoría o no dices nada, pierdes, 
+                caricachupas presenta nombres de. enseguida tú tendrás que mencionar una categoría y asi sucesivamente.
+                Por último si la palabra no está en la categoría pierdes, 
                 de lo contrario comencemos, pero no olvides seleccionar una categoría`,
              CATEGORY_MSG : `Elegiste la categoría %s, ¿Comenzamos?`,
              CARICACHUPAS_MSG: 'Caricachupas presenta nombre de %s como por ejemplo...',
-             OK_MSG: 'Excelente, continua!',
-             LOSE_MSG: 'Perdiste!, ¿Quieres jugar de nuevo?'
+             OK_MSG: 'Excelente, continúa!',
+             LOSE_MSG: '$t(AY_AY_AY_SPEECHCON) Perdiste!, ¿Quieres jugar de nuevo?',
+             GOODBYE_MSG: 'Gueim Over, Nos vemos'
         }
     }
 };
